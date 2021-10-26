8df0758c51b443d5317415f7776fd9f9f396ddfb
sbtarun <91818686+sbtarun@users.noreply.github.com>
2021-10-26 18:48:25 +0530

    Add files via upload

---
+++ ErailSort.java
@@ -0,0 +1,50 @@
+package week3.day2.assignments;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ErailSort {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+
+		driver.get("https://erail.in/");
+		Thread.sleep(2000);
+
+		driver.findElement(By.id("chkSelectDateOnly")).click();
+
+		driver.findElement(By.id("txtStationFrom")).clear();
+		Thread.sleep(1000);
+		driver.findElement(By.id("txtStationFrom")).sendKeys("Tambaram", Keys.ENTER);
+
+		driver.findElement(By.id("txtStationTo")).clear();
+		Thread.sleep(1000);
+		driver.findElement(By.id("txtStationTo")).sendKeys("Madurai Jn", Keys.ENTER);
+
+		Thread.sleep(2000);		
+
+		List<WebElement> trainList = driver.findElements(By.xpath("//td[@style=';']"));
+		List<String> trainNames = new ArrayList<String>();
+
+		for (WebElement a : trainList) {
+			trainNames.add(a.getText());
+		}
+		
+		Collections.sort(trainNames);
+		System.out.println(trainNames);
+		System.out.println("Total number of trains is: " + trainNames.size());
+	}
+}
